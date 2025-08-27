1aad84a2895393577d2335562c60cc6ebc99784a
Diwanshi Pandey <diwanshipandey331@gmail.com>
2025-08-27 14:53:59 +0530

    Update datapreprocessing1.py

--- DataPreprocessing/datapreprocessing1.py
+++ DataPreprocessing/datapreprocessing1.py
@@ -1,1 +1,10 @@
 
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:, :-1]
+Y = dataset.iloc[:,3]
+from sklearn.cross_validation import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0)
